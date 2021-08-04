a074e58e1312a94bb37e8b4104e3948d17f52739
Victoria Barkfeld <victoriacbarkfeld@gmail.com>
2021-08-03 21:51:20 -0300

    draw food

--- js/index.js
+++ js/index.js
@@ -7,6 +7,10 @@ snake[0] = {
     y: 8 * box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -20,6 +24,10 @@ function criarCobrinha(){
     }
 }
 
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box)
+}
 document.addEventListener('keydown', update);
 
 function update (event){
@@ -38,6 +46,7 @@ function iniciarJogo(){
 
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
